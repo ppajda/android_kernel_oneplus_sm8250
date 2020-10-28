37018b8e4ab0df59b15a578d8ed963b3a8f3526e
Jaegeuk Kim <jaegeuk@google.com>
2020-10-28 06:44:53 -0700

    f2fs: sync upstream inline crypto support
    Bug: 166806738
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: Idecef0d018e8b76fcf8d37227b94cbe06b5dcc52
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Pajdek <pajdek@gmail.com>

--- fs/f2fs/compress.c
+++ fs/f2fs/compress.c
@@ -1191,7 +1191,7 @@ static int f2fs_write_compressed_pages(struct compress_ctx *cc,
 		.submitted = false,
 		.io_type = io_type,
 		.io_wbc = wbc,
-		.encrypted = f2fs_encrypted_file(cc->inode),
+		.encrypted = fscrypt_inode_uses_fs_layer_crypto(cc->inode),
 	};
 	struct dnode_of_data dn;
 	struct node_info ni;
@@ -1267,8 +1267,7 @@ static int f2fs_write_compressed_pages(struct compress_ctx *cc,
 			err = f2fs_encrypt_one_page(&fio);
 			if (err)
 				goto out_destroy_crypt;
-			if (fscrypt_inode_uses_fs_layer_crypto(inode))
-				cc->cpages[i] = fio.encrypted_page;
+			cc->cpages[i] = fio.encrypted_page;
 		}
 	}
 
@@ -1307,7 +1306,7 @@ static int f2fs_write_compressed_pages(struct compress_ctx *cc,
 
 		f2fs_bug_on(fio.sbi, blkaddr == NULL_ADDR);
 
-		if (fio.encrypted && fscrypt_inode_uses_fs_layer_crypto(inode))
+		if (fio.encrypted)
 			fio.encrypted_page = cc->cpages[i - 1];
 		else
 			fio.compressed_page = cc->cpages[i - 1];
