52c5a3d367dc7640e847c382afd213367573e604
Yaroslav Furman <yaro330@gmail.com>
2021-04-14 16:25:19 -0400

    cpufreq_schedutil: remove tracing
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    [0ctobot: Adapt for 4.19]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: I5b5f3020c017777f2ea05142c036e38d9aef763b
    Signed-off-by: Pajdek <pajdek@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -299,7 +299,6 @@ static void sugov_fast_switch(struct sugov_policy *sg_policy, u64 time,
 			      unsigned int next_freq)
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
-	unsigned int cpu;
 
 	if (!sugov_update_next_freq(sg_policy, time, next_freq))
 		return;
@@ -315,10 +314,6 @@ static void sugov_fast_switch(struct sugov_policy *sg_policy, u64 time,
 	update_freq_info(policy);
 #endif /* defined(OPLUS_FEATURE_TASK_CPUSTATS) && defined(CONFIG_OPLUS_SCHED) */
 
-	if (trace_cpu_frequency_enabled()) {
-		for_each_cpu(cpu, policy->cpus)
-			trace_cpu_frequency(next_freq, cpu);
-	}
 }
 
 static void sugov_deferred_update(struct sugov_policy *sg_policy, u64 time,
@@ -529,10 +524,8 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	unsigned int prev_laf = prev_freq * util * 100 / max;
 
 	freq = choose_freq(sg_policy, prev_laf);
-	trace_sugov_next_freq_tl(policy->cpu, util, max, freq, prev_laf, prev_freq);
 #else
 	freq = map_util_freq(util, freq, max);
-	trace_sugov_next_freq(policy->cpu, util, max, freq);
 #endif
 
 	if (freq == sg_policy->cached_raw_freq && !sg_policy->need_freq_update)
