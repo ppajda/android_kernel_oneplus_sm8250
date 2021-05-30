fcf072279aea16107daae370f240b9582ad92a5b
John Galt <johngaltfirstrun@gmail.com>
2021-05-30 12:17:28 -0400

    treewide: move worker type perf affinity to cpu4
    For power efficiency and subsequent changes
    
    Signed-off-by: Pajdek <pajdek@gmail.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -844,7 +844,7 @@ static void wake_oom_reaper(struct task_struct *tsk)
 
 static int __init oom_init(void)
 {
-	oom_reaper_th = kthread_run(oom_reaper, NULL, "oom_reaper");
+	oom_reaper_th = kthread_run_perf_critical(cpu_perf_first_mask, oom_reaper, NULL, "oom_reaper");
 	return 0;
 }
 subsys_initcall(oom_init)
