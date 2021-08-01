3653740ea1f3a2effe2a7fd2c428196afb270a56
Kazuki Hashimoto <kaz205@tuta.io>
2021-08-02 00:04:51 +0900

    sched/fair: Modify capacity margins for sm8250
    This is tuned to match energy model characteristics and scheduler
    efficiency enhancements
    
    Thanks to Diep Quynh for the guidance.
    
    Co-authored-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>
    Signed-off-by: Pajdek <pajdek@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -184,9 +184,11 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
  */
 unsigned int capacity_margin				= 1280;
 unsigned int sched_capacity_margin_up[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1078}; /* ~5% margin */
+			1280, 1280, 1280, 1280, 1625, 1625, 1625, 1024
+}; /* ~20% margin for small, ~37% for big, not used for big+  */
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1205}; /* ~15% margin */
+			1024, 1024, 1024, 1024, 1796, 1796, 1796, 1442
+}; /* Not used for small, ~43% margin for big, ~29% for big+ */
 
 #ifdef CONFIG_SCHED_WALT
 /* 1ms default for 20ms window size scaled to 1024 */
