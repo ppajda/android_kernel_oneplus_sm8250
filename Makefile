c63452a6a974da9df4c0ba51c9ca8b28533af08f
Pajdek <pajdek@gmail.com>
2022-11-27 16:46:42 +0100

    Treewide: clean up
    Signed-off-by: Pajdek <pajdek@gmail.com>

--- Makefile
+++ Makefile
@@ -322,7 +322,6 @@ include scripts/subarch.include
 # Default value for CROSS_COMPILE is not to prefix executables
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 ARCH		?= arm64
-CROSS_COMPILE	?= ~/android/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)
--- OplusKernelEnvConfig.mk
+++ OplusKernelEnvConfig.mk
@@ -127,7 +127,6 @@ OPLUS_FEATURE_RT_INFO \
 OPLUS_FEATURE_MIC_VA_MIC_CLK_SWITCH
 
 ifeq ($(OPLUS_FEATURE_ADFR_KERNEL), yes)
-    $(warning add OPLUS_FEATURE_ADFR in kernel)
     ALLOWED_MCROS += OPLUS_FEATURE_ADFR
 endif
 
@@ -138,7 +137,6 @@ endif
 
 
 $(foreach myfeature,$(ALLOWED_MCROS),\
-         $(warning make $(myfeature) to be a macro here) \
          $(eval KBUILD_CFLAGS += -D$(myfeature)) \
          $(eval KBUILD_CPPFLAGS += -D$(myfeature)) \
          $(eval CFLAGS_KERNEL += -D$(myfeature)) \
