51d4cbe54c70890136ab4599d6c4d4f388c23688
Nageshwar Reddy Goli <quic_nagegoli@quicinc.com>
2024-08-27 14:05:25 +0530

    dsp: q6voice: Changing data type from %d to %ld
    "col_data->cal_data.size" is of long unsigned int type so in pr_err
    change data type of col_data->cal_data.size to %ld from %d.
    
    Change-Id: I462db4c704e9842ad3523a2ac8ef8a9f9372047f
    Signed-off-by: Nageshwar Reddy Goli <quic_nagegoli@quicinc.com>
    Signed-off-by: Pajdek <pajdek@gmail.com>

--- techpack/audio/dsp/q6voice.c
+++ techpack/audio/dsp/q6voice.c
@@ -3239,7 +3239,7 @@ static int voice_send_cvp_register_cal_cmd(struct voice_data *v)
 		 v->dev_rx.dev_id);
 
 	if (col_data->cal_data.size >= MAX_COL_INFO_SIZE) {
-		pr_err("%s: Invalid cal data size %d!\n",
+		pr_err("%s: Invalid cal data size %ld!\n",
 			__func__, col_data->cal_data.size);
 		ret = -EINVAL;
 		goto unlock;
@@ -3406,7 +3406,7 @@ static int voice_send_cvp_register_vol_cal_cmd(struct voice_data *v)
 	}
 
 	if (col_data->cal_data.size >= MAX_COL_INFO_SIZE) {
-		pr_err("%s: Invalid cal data size %d!\n",
+		pr_err("%s: Invalid cal data size %ld!\n",
 			__func__, col_data->cal_data.size);
 		ret = -EINVAL;
 		goto unlock;
